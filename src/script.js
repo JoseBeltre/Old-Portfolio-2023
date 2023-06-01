d1a57de4f616f9ede94cf1584baa88c6e3758757
JoseBeltre <126931878+JoseBeltre@users.noreply.github.com>
2023-06-01 14:57:38 -0400

    Update script.js, adding setBgColor function

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
+const header = document.querySelector('header');
 const nav = document.querySelector('.nav');
 const openNav = document.querySelector('.open-nav');
 const closeNav = document.querySelector('.close-nav');
 const aList = document.querySelector('.a_list');
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) {
+        header.classList.add('setBgColor')
+    } else {
+        header.classList.remove('setBgColor')
+    }
+})
 
 openNav.addEventListener('click', () => {
     nav.classList.toggle('visible');
