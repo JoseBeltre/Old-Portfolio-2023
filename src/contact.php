8907b94ecf4505515e4cb66596c56d84f3283b9a
Jose Beltre <552006jose@gmail.com>
2023-06-12 13:54:00 -0400

    use PHPMailer to send an email by the contact form

--- src/contact.php
+++ src/contact.php
@@ -1,6 +1,49 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-$to = '@gmail.com';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+$to = '552006jose2@gmail.com'; // Dirección de correo del destinatario
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$final_message = $name . " Ha decidido contactarte desde el formulario de tu Portafolio. Su email en caso de contacto es: " . $email . "\r\n";
+$final_message .= $name . " te comunica que: \r\n";
+$final_message .= $message;
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPDebug = 0;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+$mail->SMTPAuth = true;
+$mail->Username = '552006jose@gmail.com'; // Tu dirección de correo electrónico
+$mail->Password = 'hkamzvhvjbpflnez'; // Tu contraseña
+$mail->setFrom('noreply@gmail.com');
+$mail->addAddress($to);
+$mail->Subject = $subject;
+$mail->Body = $final_message;
+
+if ($mail->send()) {
+    echo "El correo se envió correctamente.";
+} else {
+    echo "Error al enviar el correo. " . $mail->ErrorInfo;
+}
+?>
+
+
+
+<!-- <?php
+
+$to = '552006jose@gmail.com';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
@@ -11,9 +54,6 @@ $final_message .= $name." te comunica que: \r\n";
 $final_message .= $message;
 
 $headers = "From: noreply@gmail.com\r\n";
-$headers .= "Reply-To: noreply@gmail.com\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
 
 $llego = mail($to, $subject, $final_message, $headers);
 
@@ -23,4 +63,4 @@ if ($llego) {
     echo "Error al enviar el correo.";
 }
 
-?>
+?> -->
